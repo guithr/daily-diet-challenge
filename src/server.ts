970ebb631ca7aeb50989b1c4a195800482e60ae1
guithr <guilherme.thrmatos@gmail.com>
2025-03-12 11:17:21 -0300

    chore: Install dotenv dependency

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
-app.get('/test', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+app.get('/users', async () => {
+  const user = await knex('users').insert({
+    id: crypto.randomUUID(),
+    name: 'Guilherme Matos',
+    email: 'guilherme.thrmatos@gmail.com',
+    session_id: 'teste',
+  })
 
-  return tables
+  return user
 })
 
 app
