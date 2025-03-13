17059ad2202b4c592b3a47bbaa6b99f5910d6def
guithr <guilherme.thrmatos@gmail.com>
2025-03-13 09:42:27 -0300

    fix: correct Knex import in users route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,21 +1,23 @@
 import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
-import knex from 'knex'
+import { knex } from '../database'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/', async (request, response) => {
     const createUserBodySchema = z.object({
       name: z.string().min(3, 'O nome deve ter pelo menos 3 caracteres'),
       email: z.string().email('E-mail inválido'),
+      sessionId: z.string(),
     })
 
-    const { email, name } = createUserBodySchema.parse(request.body)
+    const { email, name, sessionId } = createUserBodySchema.parse(request.body)
 
     await knex('users').insert({
       id: randomUUID(),
       name,
       email,
+      session_id: sessionId,
     })
 
     return response.status(201).send()
