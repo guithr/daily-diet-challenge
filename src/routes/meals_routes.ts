2e3f904f26031b86a99642a6c33de6cfcf67cd8f
guithr <guilherme.thrmatos@gmail.com>
2025-03-14 12:55:58 -0300

    feat: add meal listing, update, and deletion with session validation

--- src/routes/meals_routes.ts
+++ src/routes/meals_routes.ts
@@ -31,4 +31,81 @@ export async function mealsRoutes(app: FastifyInstance) {
       return response.status(201).send()
     },
   )
+
+  app.get(
+    '/',
+    {
+      preHandler: [checkIfSessionIdExist],
+    },
+    async (request, response) => {
+      const meals = await knex('meals')
+        .where({ user_id: request.user?.id })
+        .orderBy('date', 'desc')
+      return response.send({ meals })
+    },
+  )
+
+  app.get(
+    '/:mealId',
+    { preHandler: [checkIfSessionIdExist] },
+    async (request, response) => {
+      const mealsParamsSchema = z.object({
+        mealId: z.string().uuid(),
+      })
+
+      const { mealId } = mealsParamsSchema.parse(request.params)
+
+      const meal = await knex('meals').where({ id: mealId }).first()
+
+      return response.send({ meal })
+    },
+  )
+
+  app.put(
+    '/:mealId',
+    { preHandler: [checkIfSessionIdExist] },
+    async (request, response) => {
+      const mealsParamsSchema = z.object({
+        mealId: z.string().uuid(),
+      })
+
+      const { mealId } = mealsParamsSchema.parse(request.params)
+
+      const updateMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      })
+
+      const { name, description, isOnDiet, date } = updateMealBodySchema.parse(
+        request.body,
+      )
+
+      await knex('meals').where({ id: mealId }).update({
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.getTime(),
+      })
+
+      return response.status(204).send()
+    },
+  )
+
+  app.delete(
+    '/:mealId',
+    { preHandler: [checkIfSessionIdExist] },
+    async (request, response) => {
+      const mealParamsSchema = z.object({
+        mealId: z.string().uuid(),
+      })
+
+      const { mealId } = mealParamsSchema.parse(request.params)
+
+      await knex('meals').where({ id: mealId }).delete()
+
+      return response.status(204).send()
+    },
+  )
 }
